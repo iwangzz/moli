ebdc11a428a0f581517b84013cef700b7d500809
wangzz <phprince@163.com>
2016-03-03 11:27:58 +0800

    add

--- application/Admin/Lang/zh-cn/borrowtype.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'ADMIN_BORROW_TYPE_NAME' => '名称',
-    'ADMIN_BORROW_TYPE_IS_HOUSE' => '是否填写房产信息',
-    'ADMIN_BORROW_TYPE_IS_CAR' => '是否填写车辆信息',
-    'ADMIN_BORROW_TYPE_ADD_TIME' => '添加时间',
-    'ADMIN_BORROW_TYPE_STATUS' => '状态'
-);
